3b93259a10eddd2fc32d817575adff93e23ad32a
aczubik <aczubik@worldremit.com>
2019-07-11 07:40:41 +0200

    Changed the way changes are pushed to queue (how conversations are handled)

--- docker/sql/ddl/1_configure_queues.sql
+++ docker/sql/ddl/1_configure_queues.sql
@@ -1,3 +1,8 @@
+-- following created based on
+-- 1. http://rusanu.com/2007/04/25/reusing-conversations/
+-- 2. https://docs.microsoft.com/en-us/sql/database-engine/configure-windows/sql-server-service-broker?view=sql-server-2017
+-- 3. https://docs.microsoft.com/en-us/previous-versions/sql/sql-server-2008-r2/bb839489(v=sql.105)
+
 use master;
 go
 
@@ -34,6 +39,88 @@ create service SourceService_giros on queue SourceQueue_giros (ContractName_giro
 create service TargetService_giros on queue TargetQueue_giros (ContractName_giros);
 go
 
+CREATE TABLE [SessionConversations] (
+	FromService SYSNAME NOT NULL,
+	ToService SYSNAME NOT NULL,
+	OnContract SYSNAME NOT NULL,
+	Handle UNIQUEIDENTIFIER NOT NULL,
+	PRIMARY KEY (FromService, ToService, OnContract),
+	UNIQUE (Handle));
+GO
+
+CREATE PROCEDURE [usp_Send] (
+    @fromService SYSNAME, @toService SYSNAME, @onContract SYSNAME, @messageType SYSNAME, @messageBody NVARCHAR(MAX)
+)
+AS
+BEGIN
+	SET NOCOUNT ON;
+	DECLARE @handle UNIQUEIDENTIFIER;
+	DECLARE @counter INT;
+	DECLARE @error INT;
+	SELECT @counter = 1;
+	BEGIN TRANSACTION;
+	-- Will need a loop to retry in case the conversation is
+	-- in a state that does not allow transmission
+	--
+
+	WHILE (1=1)
+	BEGIN
+		-- Seek an eligible conversation in [SessionConversations]
+		--
+		SELECT @handle = Handle
+			FROM [SessionConversations]
+			WHERE FromService = @fromService
+			AND ToService = @toService
+			AND OnContract = @OnContract;
+
+		IF @handle IS NULL
+		BEGIN
+			-- Need to start a new conversation for the current @@spid
+			--
+			BEGIN DIALOG CONVERSATION @handle
+				FROM SERVICE @fromService
+				TO SERVICE @toService
+				ON CONTRACT @onContract
+				WITH ENCRYPTION = OFF;
+			INSERT INTO [SessionConversations]
+				(FromService, ToService, OnContract, Handle)
+				VALUES
+				(@fromService, @toService, @onContract, @handle);
+		END;
+
+		-- Attempt to SEND on the associated conversation
+		--
+		SEND ON CONVERSATION @handle MESSAGE TYPE @messageType (@messageBody);
+
+		SELECT @error = @@ERROR;
+		IF @error = 0
+		BEGIN
+			-- Successful send, just exit the loop
+			--
+			BREAK;
+		END
+
+		SELECT @counter = @counter+1;
+		IF @counter > 10
+		BEGIN
+			-- We failed 10 times in a row, something must be broken
+			--
+			RAISERROR (
+				N'Failed to SEND on a conversation for more than 10 times. Error %i.'
+				, 16, 1, @error) WITH LOG;
+			BREAK;
+		END
+
+		-- Delete the associated conversation from the table and try again
+		--
+		DELETE FROM [SessionConversations]
+			WHERE Handle = @handle;
+			SELECT @handle = NULL;
+	END
+	COMMIT;
+END
+GO
+
 create trigger TriggerName_giros on [dbo].giros after insert
     as
         set nocount on;
@@ -44,21 +131,29 @@ create trigger TriggerName_giros on [dbo].giros after insert
         set @row = (select id, name from inserted for json auto, without_array_wrapper);
         set @message = (select id, row = @row, tracking_type = 'inserted' from inserted for json auto, without_array_wrapper);
 
-        declare @MyDialog UNIQUEIDENTIFIER;
-        begin dialog conversation @MyDialog from service SourceService_giros to service 'TargetService_giros' on contract ContractName_giros with encryption = off;
-        send on conversation @MyDialog message type SourceMessageType_giros (@message);
-        end conversation @MyDialog;
+        EXECUTE usp_Send N'SourceService_giros', N'TargetService_giros', N'ContractName_giros', N'SourceMessageType_giros', @message
+
+--         declare @MyDialog UNIQUEIDENTIFIER;
+--         begin dialog conversation @MyDialog from service SourceService_giros to service 'TargetService_giros' on contract ContractName_giros with encryption = off;
+--         send on conversation @MyDialog message type SourceMessageType_giros (@message);
+--         end conversation @MyDialog;
 go
 
---insert into giros values (newid(), 'arek');
---
--- DECLARE @counter int
--- SET @counter = 1300
--- WHILE @counter < 1400 BEGIN
---     insert into giros values(@counter, concat('arek_', @counter));
---     SET @counter = @counter + 1
---     waitfor delay '00:00:00.100'
--- END
--- GO
+use remit;
+go
 
--- select * from TargetQueue_giros;
+
+-- EXECUTE usp_Send N'SourceService_giros', N'TargetService_giros', N'ContractName_giros', N'SourceMessageType_giros', 'terefere'
+
+
+DECLARE @counter int
+SET @counter = 5000
+WHILE @counter < 50000 BEGIN
+    insert into giros values (newid(), concat('arek', @counter));
+    SET @counter = @counter + 1
+    -- waitfor delay '00:00:00.010'
+END
+GO
+
+select * from TargetQueue_giros;
+select * from sys.transmission_queue;
--- queue-to-kafka-connector/src/main/java/com/worldremit/QueueToKafkaConnectorApplication.java
+++ queue-to-kafka-connector/src/main/java/com/worldremit/QueueToKafkaConnectorApplication.java
@@ -13,6 +13,7 @@ import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -20,6 +21,7 @@ import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaAdmin;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.stereotype.Component;
 
 import javax.persistence.*;
 import java.io.IOException;
@@ -28,7 +30,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 
-@Table(name = "TargetQueue")
 @Entity
 @Data
 class Event {
@@ -49,28 +50,9 @@ class EventValue {
     @JsonProperty("tracking_type")
     private String trackingType;
 
-    public EventValue() {
-    }
-
-    public EventValue(UUID id, String row, String trackingType) {
-        this.id = id;
-        this.row = row;
-        this.trackingType = trackingType;
-    }
-
     public String getRow() {
         return row;
     }
-}
-
-interface EventRepository extends JpaRepository<Event, String> {
-
-    @Query(nativeQuery = true, value = "waitfor(\n" +
-            "        RECEIVE top (1) conversation_handle,service_name,message_type_name,message_body,message_sequence_number\n" +
-            "        FROM [TargetQueue]\n" +
-            "        ), timeout 3000")
-    List<Event> findEvents();
-
 }
 
 @Configuration
@@ -119,62 +101,58 @@ class KafkaProducerConfig {
     }
 }
 
+class MsSqlQueueListener {
 
-@SpringBootApplication
-public class QueueToKafkaConnectorApplication {
+    private static final String END_CONVERSATION_MESSAGE_TYPE = "http://schemas.microsoft.com/SQL/ServiceBroker/EndDialog";
 
     private static final String RECEIVE_QUERY = "waitfor(\n" +
             "        RECEIVE top (1) conversation_handle,service_name,message_type_name,message_body,message_sequence_number\n" +
             "        FROM [TargetQueue_giros]\n" +
             "        ), timeout 3000";
-    public static void main(String[] args) {
-        SpringApplication.run(QueueToKafkaConnectorApplication.class, args);
-    }
 
-    @Autowired
-    private EventRepository repository;
-    @Autowired
     private EntityManagerFactory emf;
-    @Autowired
     private KafkaTemplate<String, String> kafkaTemplate;
 
-    @Bean
-    ApplicationRunner applicationRunner() {
-        return args -> {
+    private final String sqlServerQueueName;
 
-            EntityManager em = null;
-            try {
-                em = emf.createEntityManager();
-
-                while (1==1) {
-
-                    EntityTransaction transaction = em.getTransaction();
-                    transaction.begin();
-
-                    try {
-                        List<Event> events = em.createNativeQuery(RECEIVE_QUERY, Event.class).getResultList();
-                        for (Event event : events) {
-                            processEvent(event);
-                        }
-                        transaction.commit();
-                        em.clear();
-                    } catch (Exception exception) {
-                        transaction.rollback();
-                        exception.printStackTrace();
-                    }
-                }
-
-            } catch (Exception exception) {
-                if (em != null) {
-                    em.close();
-                }
-            }
-        };
+    public MsSqlQueueListener(String sqlServerQueueName, EntityManagerFactory emf, KafkaTemplate<String, String> kafkaTemplate) {
+        this.sqlServerQueueName = sqlServerQueueName;
+        this.emf = emf;
+        this.kafkaTemplate = kafkaTemplate;
     }
 
-    private static final String END_CONVERSATION_MESSAGE_TYPE = "http://schemas.microsoft.com/SQL/ServiceBroker/EndDialog";
+    public void waitForMessages() {
+        EntityManager em = null;
+        try {
+            em = emf.createEntityManager();
+
+            while (1==1) {
+
+                EntityTransaction transaction = em.getTransaction();
+                transaction.begin();
+
+                try {
+                    List<Event> events = em.createNativeQuery(RECEIVE_QUERY, Event.class).getResultList();
+                    for (Event event : events) {
+                        processEvent(event);
+                    }
+                    transaction.commit();
+                    em.clear();
+                } catch (Exception exception) {
+                    transaction.rollback();
+                    exception.printStackTrace();
+                }
+            }
+
+        } catch (Exception exception) {
+            if (em != null) {
+                em.close();
+            }
+        }
+    }
 
     private void processEvent(Event event) throws IOException {
+
         if (event.getMessageTypeName().equals(END_CONVERSATION_MESSAGE_TYPE)) {
             return;
         }
@@ -188,6 +166,29 @@ public class QueueToKafkaConnectorApplication {
 
         kafkaTemplate.send("giros", eventValue.getRow());
 
+    }
+}
+
+@SpringBootApplication
+public class QueueToKafkaConnectorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QueueToKafkaConnectorApplication.class, args);
+    }
+
+    @Autowired
+    private MsSqlQueueListener msSqlQueueListener;
+
+    @Bean
+    public MsSqlQueueListener girosListener(EntityManagerFactory emf, KafkaTemplate<String, String> kafkaTemplate) {
+        return new MsSqlQueueListener("TargetQueue_giros", emf, kafkaTemplate);
+    }
+
+    @Bean
+    ApplicationRunner applicationRunner() {
+        return args -> {
+            msSqlQueueListener.waitForMessages();
+        };
     }
 
 }
